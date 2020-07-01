2131de846f70c8ae9870c4dda95e6d10fea8584d
aryabariz <aryawicak12@gmail.com>
2020-07-01 23:30:53 +0700

    route test page

--- src/app/router/routerPath.js
+++ src/app/router/routerPath.js
@@ -4,6 +4,7 @@ import routerBeranda from './routerBeranda';
 import routerMenuPeta from './peta/routerMenuPeta';
 import routerMenuInformasi from './informasi/routerMenuInformasi';
 import Agenda from '../layout/informasi/Tentang/halTentang';
+import testpage from '../layout/informasi/Agenda/agendaInternal'
 
 function routerPath(){
     return(     
@@ -12,6 +13,7 @@ function routerPath(){
                 <Route exact path="/menuPeta" component={routerMenuPeta}/>
                 <Route exact path="/menuInformasi" component={routerMenuInformasi}/>
                 <Route exact path="/menuInformasi/agenda/internal" component={Agenda}/>
+                <Route exact path="/testpage" component={routerMenuInformasi}/>
             </Switch>
         );
 }
