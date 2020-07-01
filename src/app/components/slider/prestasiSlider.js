e6ae6ec849c21843b979efb808b59d318ec9d886
aryabariz <aryawicak12@gmail.com>
2020-07-02 01:25:44 +0700

    ulang

--- src/app/components/slider/prestasiSlider.js
+++ src/app/components/slider/prestasiSlider.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Slider from 'react-slick';
 import './sliderStyle.css';
-import apiurl from '../../api';
+import apiurl from '../api';
 
 
 export default class kontenSlider extends Component {
