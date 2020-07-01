54bb1d1c093c3a4d7fcb0506144379775f3c81b7
aryabariz <aryawicak12@gmail.com>
2020-07-01 23:55:39 +0700

    ulang

--- src/app/components/slider/agendaSlider.js
+++ src/app/components/slider/agendaSlider.js
@@ -55,7 +55,7 @@ export default class agendaSlider extends Component{
             <div class="zonaSAgenda">
                   
                   <Slider {...settings}>
-                        {datas.filter(filterAgenda => filterAgenda.agenda_type === {tipe}).map(Agenda =>(
+                        {datas.filter(filterAgenda => filterAgenda.agenda_type === tipe).map(Agenda =>(
                     <div key={Agenda.id}>
                      <div class="gmbrAgenda"  > 
                      <img  class="ui fluid image" src={Agenda.agenda_photo} alt="poster"/>
